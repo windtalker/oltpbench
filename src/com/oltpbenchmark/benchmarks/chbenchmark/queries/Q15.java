0c690310e872cbe8543ef2d0a7edad407fa818fe
xufei <xufeixw@mail.ustc.edu.cn>
2020-01-08 10:27:57 +0800

    fix bug

--- src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q15.java
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q15.java
@@ -25,11 +25,11 @@ import java.sql.SQLException;
 public class Q15 extends GenericQuery {
     public final SQLStmt query_stmt = new SQLStmt(
             "select su_suppkey, su_name, su_address, su_phone, total_revenue"
-                    + " from supplier, (select s_su_suppkey as supplier_no, sum(ol_amount) as total_revenue from order_line,stock where ol_i_id = s_i_id and "
-                    + " ol_supply_w_id = s_w_id and ol_delivery_d >= '2007-01-02 00:00:00.000000' group by s_su_suppkey) as revenue"
+                    + " from supplier, (select mod((s_w_id * s_i_id),10000) as supplier_no, sum(ol_amount) as total_revenue from order_line,stock where ol_i_id = s_i_id and "
+                    + " ol_supply_w_id = s_w_id and ol_delivery_d >= '2007-01-02 00:00:00.000000' group by supplier_no) as revenue"
                     + " WHERE su_suppkey = supplier_no and total_revenue = (select max(total_revenue) from "
-                    + " (select s_su_suppkey as supplier_no,sum(ol_amount) as total_revenue from order_line, stock where ol_i_id = s_i_id"
-                    + " and ol_supply_w_id = s_w_id and ol_delivery_d >= '2007-01-02 00:00:00.000000' group by s_su_suppkey) as revenue)"
+                    + " (select mod((s_w_id * s_i_id),10000) as supplier_no,sum(ol_amount) as total_revenue from order_line, stock where ol_i_id = s_i_id"
+                    + " and ol_supply_w_id = s_w_id and ol_delivery_d >= '2007-01-02 00:00:00.000000' group by supplier_no) as revenue)"
                     + " order by su_suppkey"
     );
 
